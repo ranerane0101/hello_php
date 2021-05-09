038136138cf079578f553322737b164599f28eed
ranerane0101 <runrunrun4649@icloud.com>
2021-05-09 17:27:23 +0900

    Takarasagasi

--- index.php
+++ index.php
@@ -6,27 +6,34 @@ echo "<h1>宝探し</h1>";
 echo "<p>マップ上の適当な場所をクリック！</p>";
 //ゲーム状態を確認する
 $stat = $_SESSION['stat'];
+//新しく追記
+ // $m = 0;
+// if(isset($_GET['m'])){
+//   $m = intval($_GET['m']);
+// }
+
 if(!$stat || $_GET['m'] == "reset")
 {
   init_game();
   draw_map();
+  $m = intval($_GET['m']);
   exit;
 }
-//when panels would open
+//パネルを開くとき
 if($_GET['m'] = 'click')
 {
   click_panel();
 }
-//game palameter starts
+//ゲームパラメーターの初期化
 function init_game()
 {
 //ゲームの初期化
   $_SESSION['stat']    =    'playing';
   $_SESSION['turn']    =    0;
-  //treasure returns
+  //宝の位置を設定する
   $_SESSION['treasure_x'] = rand(0,8);
   $_SESSION['treasure_y'] = rand(0,8);
-  //maps returns
+  //マップを初期化する
   for($y = 0; $y < 9; $y++){
     for($x = 0; $x < 9; $x++){
       $_SESSION['map'][$y][$x] ="*";
@@ -35,24 +42,21 @@ function init_game()
   }
 
 }
-//Map draws
+//マップの描画、tableタグの表示
 function draw_map()
 {
   $map = $_SESSION['map'];
   $s = "<table border='1'>";
-  for($y = 0; $y < 9; $y++)
-  {
+  for($y = 0; $y < 9; $y++){
     $s .= "<tr>";
-    for($x = 0; $x < 9; $x++)
-    {
+    for($x = 0; $x < 9; $x++){
       $v = $_SESSION["map"][$y][$x];
       $color = "#FFFFFF";
-      if($v == "*")
-      {
-        $v = "<a href='?m=click&x=$x$y&y=$y'>*</a>";
+      if($v == "*"){
+        $v = "<a href='?m=click&x=$x&$y=$y'>*</a>";
         $color = "#C0C0C0";
       }
-      $s .="<td width='24' aligh='center'bgcolor='$color'>";
+      $s .="<td width='24' align='center' bgcolor='$color'>";
       $s .="$v</td>";
     }
     $s .= "</tr>";
@@ -62,11 +66,21 @@ function draw_map()
   echo "<p>今は、{$_SESSION['turn']}手目です。</p>";
   echo "<p><a href='?m=reset'>*Restart*</a></p>";
 }
-//When panels is clicked
+//パネルをクリックしたときの処理
 function click_panel()
 {
-  $x = intval($_GET['x']);
-  $y = intval($_GET['y']);
+  //$x = intval(filter_input(INPUT_GET,'x',FILTER_VALIDATE_INT));
+  //$y = intval(filter_input(INPUT_GET,'y',FILTER_VALIDATE_INT));
+//新しく追記
+  $x = 0;
+  if(isset($_GET['x'])){
+    $x = intval($_GET['x']);
+  }
+  $y = 0;
+  if(isset($_GET['y'])){
+    $y = intval($_GET['y']);
+  }
+
   $treasure_x = $_SESSION['treasure_x'];
   $treasure_y = $_SESSION['treasure_y'];
   $_SESSION['turn']++;
